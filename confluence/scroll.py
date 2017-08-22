d24d885d86c8d97db10fe6632dcc34edbd53c239
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-08-22 17:18:51 +0200

    commented out content to pass flake8

--- confluence/scroll.py
+++ confluence/scroll.py
@@ -1,52 +1,52 @@
-from confluence import Confluence
-
-conf = Confluence('profile')
-space_key = 'space key'
-page = 'title of root page'
-root_page_id = conf.get_page_id(space_key, page)
-
-
-def space_attributes_to_set(space_key):
-    """Query whether user wants to add, remove, set or clear attributes."""
-    user_choice_action = input('Do you want to [s]et or [c]lear attributes?')
-    while user_choice_action != 's' or 'c':
-        print(f"Your choice {user_choice_action} is not linked to an action.\n"
-              f" You must choose among [s]et, [a]dd, [r]emove and [c]lear.")
-
-    # Query available attributes and their values
-    AvailableAttributeList = conf.connection.get(conf.base_url + f'/rest/scroll-versions/1.0/attribute/{space_key}')
-
-    for item in AvailableAttributeList:
-        # foreach($Attribute in $Response)
-        # {
-        #     foreach($Value in $Attribute.values)
-        # {
-            # $OutObj | Add - Member - MemberType: NoteProperty - Name:'Attributename' - Value:$Attribute.name
-            # $OutObj | Add - Member - MemberType: NoteProperty - Name:'Attributeid' - Value:$Attribute.id
-            # $OutObj | Add - Member - MemberType: NoteProperty - Name:'Valuename' - Value:$Value.name
-            # $OutObj | Add - Member - MemberType: NoteProperty - Name:'Valueid' - Value:$Value.id
-            # $AvailableAttributeList += $OutObj
-            # }
-        pass
-
-        print(f"Found the following attributes for space {space_key}:")
-        for num, attribute, value in AvailableAttributeList:
-            print(f"{num}: {attribute}: {value}")
-
-    UserChoice = input("Please enter a comma-separated list of attributes (by index) you want to set:")
-    while not UserChoice:
-        print("ERROR: You must specify at least a single attribute value to set.")
-    TargetAttributeList = input()
-
-    for item in UserChoice.split(","):
-        # $Index = [int]::Parse($Item.Trim())
-        # if ($Index -lt 0 - or $Index -gt $AvailableAttributeList.Count)
-        #     throw "ERROR: Input value '$Index' is out of range."
-        #
-        # $TargetAttributeList += $AvailableAttributeList[$Index]
-        pass
-
-    return UserChoice, TargetAttributeList
+# from confluence import Confluence
+#
+# conf = Confluence('profile')
+# space_key = 'space key'
+# page = 'title of root page'
+# root_page_id = conf.get_page_id(space_key, page)
+#
+#
+# def space_attributes_to_set(space_key):
+#     """Query whether user wants to add, remove, set or clear attributes."""
+#     user_choice_action = input('Do you want to [s]et or [c]lear attributes?')
+#     while user_choice_action != 's' or 'c':
+#         print(f"Your choice {user_choice_action} is not linked to an action.\n"
+#               f" You must choose among [s]et, [a]dd, [r]emove and [c]lear.")
+#
+#     # Query available attributes and their values
+#     AvailableAttributeList = conf.connection.get(conf.base_url + f'/rest/scroll-versions/1.0/attribute/{space_key}')
+#
+#     for item in AvailableAttributeList:
+#         # foreach($Attribute in $Response)
+#         # {
+#         #     foreach($Value in $Attribute.values)
+#         # {
+#             # $OutObj | Add - Member - MemberType: NoteProperty - Name:'Attributename' - Value:$Attribute.name
+#             # $OutObj | Add - Member - MemberType: NoteProperty - Name:'Attributeid' - Value:$Attribute.id
+#             # $OutObj | Add - Member - MemberType: NoteProperty - Name:'Valuename' - Value:$Value.name
+#             # $OutObj | Add - Member - MemberType: NoteProperty - Name:'Valueid' - Value:$Value.id
+#             # $AvailableAttributeList += $OutObj
+#             # }
+#         pass
+#
+#         print(f"Found the following attributes for space {space_key}:")
+#         for num, attribute, value in AvailableAttributeList:
+#             print(f"{num}: {attribute}: {value}")
+#
+#     UserChoice = input("Please enter a comma-separated list of attributes (by index) you want to set:")
+#     while not UserChoice:
+#         print("ERROR: You must specify at least a single attribute value to set.")
+#     TargetAttributeList = input()
+#
+#     for item in UserChoice.split(","):
+#         # $Index = [int]::Parse($Item.Trim())
+#         # if ($Index -lt 0 - or $Index -gt $AvailableAttributeList.Count)
+#         #     throw "ERROR: Input value '$Index' is out of range."
+#         #
+#         # $TargetAttributeList += $AvailableAttributeList[$Index]
+#         pass
+#
+#     return UserChoice, TargetAttributeList
     #
     # 'c'
     #
