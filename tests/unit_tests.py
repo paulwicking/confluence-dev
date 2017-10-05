2ce5ad522ede958901e7bae23851b556d2c17bcf
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-10-05 16:55:57 +0200

    Need to clean up unit tests connecting to localhost

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -24,7 +24,7 @@ def test_confluence_init_method(mock_connection_valid):
     :param mock_connection_valid: Intercepted by unittest.mock.patch and replaces connection_valid() during test..
     """
     mock_connection_valid.return_value = True
-    conf = confluence.Confluence(profile='pycontribs-test')
+    conf = confluence.Confluence(profile='general')
 
     assert conf is not None
 
@@ -47,7 +47,7 @@ def test_confluence_init_method_ok():
         return return_object
 
     with mock.patch.object(requests.Session, 'get', new=mock_get_ok):
-        conf = confluence.Confluence(profile='pycontribs-test')
+        conf = confluence.Confluence(profile='general')
         assert conf.connection is not None
         assert conf.connection_valid()
 
@@ -60,9 +60,18 @@ def test_confluence_init_method_fails():
         return return_object
 
     with mock.patch.object(requests.Session, 'get', new=mock_get_fail):
-        conf = confluence.Confluence(profile='pycontribs-test')
+        conf = confluence.Confluence(profile='general')
         assert not conf.connection_valid()
         assert conf.connection is None
+
+
+def test_connection_valid_raises_exceptions():
+    """Test that connection_valid() calls requests.raise_for_status.
+
+    This test mocks the session created with requests.Session and asserts that the connection check fails.
+    """
+    with pytest.raises(requests.exceptions.RequestException):
+        conf = confluence.Confluence()
 
 
 def test_get_blog_entry_with_clean_results():
@@ -84,6 +93,24 @@ def test_get_blog_entry_with_clean_results():
 #     assert expected_response in actual_response
 
 
+@patch('xmlrpc.client.ServerProxy')
+def test_pending_deprecation_warnings(mock_server):
+    """Test that calling XMLRPC methods issue pending deprecation warnings.
+
+    When XML-RPC based methods are called, the pending_deprecation wrapper function issues a warning.
+
+    :param mock_server: Intercepted by unittest.mock.patch and replaces xmlrpclib during test.
+    """
+    mock_server.confluence2.getSpaces.return_value = True
+    conf = confluence.Confluence()
+    with warnings.catch_warnings(record=True) as warn:
+        warnings.simplefilter('always')
+        conf.getSpaces()
+
+        assert issubclass(warn[-1].category, PendingDeprecationWarning)
+        assert "use the Atlassian REST API in future versions" in str(warn[-1].message)
+
+
 def test_check_response_returns_false_on_invalid_response(mock_conf):
     """"Tests that invalid response checks return False.
 
@@ -96,6 +123,26 @@ def test_check_response_returns_false_on_invalid_response(mock_conf):
     # assert conf.check_response(call_that_causes_404) is False
     # assert conf.check_response(call_that_returns_empty_results_list) is False
     pass
+
+
+def test_check_reponse():
+    def mock_get_ok(mock_self, mock_response):
+        """Mock method to pass as the mock.patch.object in this test.
+
+        :param mock_self: Mock the self parameter of connection_valid().
+        :param mock_response: Mock the response parameter of connection_valid().
+        :return: Mock object with property ok = True.
+        """
+        return_object = mock.Mock()
+        return_object.ok = True
+        return return_object
+
+    with mock.patch.object(requests.Session, 'get', new=mock_get_ok):
+        conf = confluence.Confluence()
+        response_is_none = None
+        mock_response = mock.Mock()
+
+        assert conf.check_response(response_is_none) is False
 
 
 def test_check_response_returns_true_on_valid_response(mock_conf):
